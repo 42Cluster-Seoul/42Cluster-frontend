8f11b6745def9ea7f561defd480bd872666c1f75
luckylooky2 <luckylooky2@naver.com>
2024-03-19 16:59:51 +0900

    Fix: manage backend state in redux store

--- public/app/core/components/AppChrome/TopBar/TopSearchBar.tsx
+++ public/app/core/components/AppChrome/TopBar/TopSearchBar.tsx
@@ -1,13 +1,14 @@
 import { css } from '@emotion/css';
 // import { cloneDeep } from 'lodash';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { GrafanaTheme2, locationUtil, textUtil } from '@grafana/data';
 import { Dropdown, ToolbarButton, useStyles2 } from '@grafana/ui';
 import { config } from 'app/core/config';
 import { contextSrv } from 'app/core/core';
-import { useSelector } from 'app/types';
+import { fourtyTwoClusterBackendFetching } from 'app/features/fourtyTwoClusterBackend/state/reducers';
+import { useSelector, useDispatch } from 'app/types';
 
 import { Branding } from '../../Branding/Branding';
 // import { enrichHelpItem } from '../MegaMenu/utils';
@@ -24,8 +25,21 @@ import { TopSearchBarSection } from './TopSearchBarSection';
 export const TopSearchBar = React.memo(function TopSearchBar() {
   const styles = useStyles2(getStyles);
   const navIndex = useSelector((state) => state.navIndex);
+  const backendState = useSelector((state) => state.fourtyTwoClusterBackend);
   const location = useLocation();
-  const [serviceName, setServiceName] = useState('');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch(
+        fourtyTwoClusterBackendFetching({
+          serviceId: 0,
+          serviceName: '42 Morning glory',
+          dashboardUID: 'a87fb0d919ec0ea5f6543124e16c42a5',
+        })
+      );
+    }, 500);
+  }, [dispatch]);
 
   // const helpNode = cloneDeep(navIndex['help']);
   // const enrichedHelpNode = helpNode ? enrichHelpItem(helpNode) : undefined;
@@ -36,21 +50,21 @@ export const TopSearchBar = React.memo(function TopSearchBar() {
     homeUrl = textUtil.sanitizeUrl(locationUtil.getUrlForPartial(location, { forceLogin: 'true' }));
   }
 
-  useEffect(() => {
-    setTimeout(() => {
-      setServiceName('42 Morning glory');
-    }, 500);
-  }, []);
-
   return (
     <div className={styles.layout}>
       <TopSearchBarSection>
         <a className={styles.logo} href={homeUrl} title="Go to home">
           <Branding.MenuLogo className={styles.img} />
-          <div className={styles.serviceName}>
-            <b>{serviceName ? serviceName : ''}</b>
-          </div>
         </a>
+        {backendState.isValid ? (
+          <a className={styles.logo} href={homeUrl} title="Go to home">
+            <div className={styles.serviceName}>
+              <b>{backendState.serviceName}</b>
+            </div>
+          </a>
+        ) : (
+          <span>loading...</span>
+        )}
         <OrganizationSwitcher />
       </TopSearchBarSection>
 
